7f482ebe407ea38cad77c0577d22d5f55a971847
vasilakhan0805 <azizzkhan1997@gmail.com>
2023-03-06 12:49:14 +0500

    header finished

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from "react";
 import Navbar from "./Navbar";
+import Header from "./Header/index";
 
 class Home extends Component{
     render(){
         return(
             <div>
                 <Navbar/>
+                <Header/>
+                
             </div>
         )
     }
